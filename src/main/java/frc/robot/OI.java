3717dd6a79f5bdff491dd3d39f5e08b63e0afb6a
Emily Speckhals <speckhalse@region18.org>
2018-11-15 18:49:57 -0500

    added button for SpinUp

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -7,7 +7,21 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import frc.robot.commands.SpinUp;
 
 public class OI {
-  
+    //creates controller and button to "launch" (right now just turn on 1 motor)
+    public Joystick controller;
+    public JoystickButton launch;
+
+  public OI() {
+      //specify the port and button to use
+      controller = new Joystick(RobotMap.USB_CONTROLLER);
+      launch = new JoystickButton(controller, RobotMap.SPIN_UP_BUTTON);
+      
+      //runs SpinUp command while the above specified button is held down
+      launch.whileHeld(new SpinUp());
+  }
 }
--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -9,6 +9,7 @@ package frc.robot;
 
 
 public class RobotMap {
+  public static final int USB_CONTROLLER = 2;
 
   //this is the PWM number of the motor controller for the motor we want to move
   public static final int PWM_TOP_RIGHT = 2;
@@ -16,6 +17,9 @@ public class RobotMap {
   //by storing the launch speed here it is easier to change b/c you don't have to 
   //find where you used it in the code, can be set b/w 0-1
   public static final double LAUNCH_SPEED = 0.5;
+
+  //stores button # for spinning up, button 1 is A (green)
+  public static final int SPIN_UP_BUTTON = 1;
 
   
 }
